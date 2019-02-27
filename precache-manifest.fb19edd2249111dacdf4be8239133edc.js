05c2c9bc0edb7222f568064e14d6533f56954229
ArtemChubatyi <stgmdota@gmail.com>
2019-02-27 23:03:16 +0200

    Updates

---
+++ precache-manifest.fb19edd2249111dacdf4be8239133edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.a03dc12c.js"
+  },
+  {
+    "revision": "8b58e73ce0e3ae264f2b",
+    "url": "/test/static/js/main.8b58e73c.chunk.js"
+  },
+  {
+    "revision": "0174de6bf9d6de60568f",
+    "url": "/test/static/js/2.0174de6b.chunk.js"
+  },
+  {
+    "revision": "8b58e73ce0e3ae264f2b",
+    "url": "/test/static/css/main.ba3dd7a4.chunk.css"
+  },
+  {
+    "revision": "9f9816a8881aed49fb5b75003cbeca0c",
+    "url": "/test/index.html"
+  }
+];
